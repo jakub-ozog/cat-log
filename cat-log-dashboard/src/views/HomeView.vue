e8dc9c9fd1e4c7e5a20e139b49f8c9d91e688a25
jakub-ozog <jakubjanozog@gmail.com>
2025-06-20 20:55:25 +0200

    feat: add shadcn setup

--- cat-log-dashboard/src/views/HomeView.vue
+++ cat-log-dashboard/src/views/HomeView.vue
@@ -1,8 +1,10 @@
 <script setup>
+import { Button } from '@/components/ui/button'
 </script>
 
 <template>
   <main>
     <h1 class="text-4xl font-bold ">Home View</h1>
+    <Button>Click here</Button>
   </main>
 </template>
